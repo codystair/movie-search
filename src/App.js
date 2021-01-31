29fe533e14cac6db37a278101bf40879d5824c5e
Cody Stair <ccstair27@gmail.com>
2021-01-31 13:18:02 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { inputValue: "", searchResults: [] };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ inputValue: event.target.value });
+  }
+
+  handleSubmit(event) {
+    const key = "a932735c";
+    const apiUrl = `http://www.omdbapi.com/?apikey=${key}&s=`;
+    fetch(apiUrl + this.state.inputValue)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.Search);
+        this.setState({ searchResults: data.Search });
+      });
+    event.preventDefault();
+  }
+
+  render() {
+    const results = this.state.searchResults.map((entry) => {
+      return (
+        <li>
+          Title: {entry.Title}, Year: {entry.Year}, IMDB id: {entry.imdbID}
+        </li>
+      );
+    });
+
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="search">Search by title </label>
+          <input
+            type="text"
+            value={this.state.inputValue}
+            onChange={this.handleChange}
+            name="search"
+          />
+          <input type="submit" value="Submit" />
+        </form>
+
+        <ul>{results}</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
